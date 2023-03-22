8f8d55d2146cedd9431bfcf9e70214a238a23a01
Munkh-Orgil Batchuluun <munkh1151@gmail.com>
2023-03-22 17:40:31 +0800

    delete args file

--- args.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-argParser = argparse.ArgumentParser()
-argParser.add_argument("-batch_size", "--batch_size", help="batch_Size")
-args = argParser.parse_args()
-print("args=%s" % args)
-print("args.batch_size=%s" % args.batch_size)
-print("args.batch_size type=", list(args.batch_size))
